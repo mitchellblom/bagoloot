8ce1d559c4ccd60741081df33f714e93bad692ae
Mitchell Blom <mitchellblom@gmail.com>
2017-07-18 11:15:07 -0500

    first

---
+++ BagOLoot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BagOLoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
